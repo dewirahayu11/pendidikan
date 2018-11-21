2bb1ae552908c646c69876fa6945341b5547dc7e
sallyrosiani <sallyrosiani@gmail.com>
2018-11-21 18:02:30 +0700

    Update application/views/OurTeam.php
    update ourteam

---
+++ application/views/OurTeam.php
@@ -0,0 +1,231 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/fontawesome/css/fontawesome-all.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/custom.css">
+
+    <!-- Extra plugin css -->
+    <link href="<?php echo base_url(); ?>/assets/vendors/owl-carousel/owl.carousel.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>/assets/css/style.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>/assets/css/responsive.css" rel="stylesheet">
+    <!-- Rev slider css -->
+        <link href="<?php echo base_url(); ?>/assets/vendors/revolution/css/settings.css" rel="stylesheet">
+        <link href="<?php echo base_url(); ?>/assets/vendors/revolution/css/layers.css" rel="stylesheet">
+        <link href="<?php echo base_url(); ?>/assets/vendors/revolution/css/navigation.css" rel="stylesheet">
+        <link href="<?php echo base_url(); ?>/assets/vendors/animate-css/animate.css" rel="stylesheet">
+    <!-- Icon css link -->
+        <link href="<?php echo base_url(); ?>/assets/css/font-awesome.min.css" rel="stylesheet">
+        <link href="<?php echo base_url(); ?>/assets/vendors/elegant-icon/style.css" rel="stylesheet">
+        <link href="<?php echo base_url(); ?>/assets/vendors/themify-icon/themify-icons.css" rel="stylesheet">
+
+    <title>LearningSpace</title>
+  </head>
+  <body>
+    <header class="main_menu_area">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light" id="mainNav">
+                <a class="navbar-brand" href="#"><img src="<?php echo base_url(); ?>/assets/images/logo.png" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="about">About Us</a></li>
+                        <li class="nav-item"><a class="nav-link" href="OurFitur">Our Product</a></li>
+                        <li class="nav-item"><a class="nav-link" href="OurTeam">Our Team</a></li>
+                        <li class="nav-item"><a class="nav-link" href="Kontak">Contact</a></li>
+                    </ul>
+                    <ul class="navbar-nav justify-content-end">
+                        <li><a href="#"><i class="icon_search"></i></a></li>
+                        <li><a href="#"><i class="icon_lock"></i></a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header> 
+
+    <div class="container-fluid banner pb-5">
+      <div class="container">
+        <div class="row py-5">
+          <div class="col-md-12 text-white">
+            <h3 class="text-center m-4">"Kebersamaan adalah permulaan. Menjaga bersama adalah kemajuan. Bekerja bersama adalah keberhasilan.-Henry Ford-</h3>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- END BANNER -->
+     <div class="container">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <hr>
+          <h1>Our Team</h1>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="card mt-2">
+            <img class="card-img-top" src="<?php echo base_url(); ?>/assets/images/nama_shella.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Shella Intia</h5>
+              <br>27 Januari 1998
+              <p class="card-text">Jurusan Teknik Informatika angkatan 2016 Institut Teknologi Sumatera.</p>
+              
+              
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card mt-2">
+            <img class="card-img-top" src="<?php echo base_url(); ?>/assets/images/nama_sally.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Sally Rosiani</h5>
+              <br>04 Juni 1998
+              <p class="card-text">Jurusan Teknik Informatika angkatan 2016 Institut Teknologi Sumatera.</p>
+
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card mt-2">
+            <img class="card-img-top" src="<?php echo base_url(); ?>/assets/images/nama_sartiah.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Sartiah</h5>
+              <br>10 Maret 1999
+              <p class="card-text">Jurusan Teknik Informatika angkatan 2016 Institut Teknologi Sumatera.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card mt-2">
+            <img class="card-img-top" src="<?php echo base_url(); ?>/assets/images/nama_oktri.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Muhammad Oktri S.</h5>
+              <br>08 Oktober 1997
+              <p class="card-text">Jurusan Teknik Informatika angkatan 2016 Institut Teknologi Sumatera.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card mt-2">
+            <img class="card-img-top" src="<?php echo base_url(); ?>/assets/images/nama_peik.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Muhammad Syafi'i</h5>
+              <br>29 Desember 1997
+              <p class="card-text">Jurusan Teknik Informatika angkatan 2016 Institut Teknologi Sumatera.</p>
+
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card mt-2">
+            <img class="card-img-top" src="<?php echo base_url(); ?>/assets/images/nama_agus.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Agustino Rahmawan</h5>
+              <br>08 Agustus 1998
+              <p class="card-text">Jurusan Teknik Informatika angkatan 2016 Institut Teknologi Sumatera.</p>
+
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card mt-2">
+            <img class="card-img-top" src="<?php echo base_url(); ?>/assets/images/nama_vicko.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Vicko Pratama</h5>
+              <br>21 Oktober 1998
+              <p class="card-text">Jurusan Teknik Informatika angkatan 2016 Institut Teknologi Sumatera.</p>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- END FEATURES -->
+    
+    <!-- END FEATURES -->
+    <div class="container-fluid py-5 text-white footer">
+      <div class="conteiner">
+        <div class="row">
+          <div class="col-md-5">
+            <h3>Lokasi</h3>
+            <p>Jalan Ryacudu Gang Hasan raya <br>Kel. Korpri Raya Kec. Sukarame <br>Bandar Lampung, Lampung</p>
+            <p>phone (021) 123-456 - 123-123 <br>learningspace@gmail.com</p>
+          </div>
+          <div class="col-md-5">
+            <div class="row">
+              <div class="col-md-6">
+                <h5>About Learning<i>space</i></h5>
+                <ul class="list-group">
+                  <li class="list-group-item"><a href="about">Tentan Kami</a></li>
+                  <li class="list-group-item"><a href="ourteam">Tim Kami</a></li>
+                </ul>
+              </div>
+              <div class="col-md-6">
+                <h5>Others Link</h5>
+                <ul class="list-group">
+                  <li class="list-group-item"><a href="registrasi">Registrasi</a></li>
+                  <li class="list-group-item"><a href="panduan">Panduan</a></li>
+                  <li class="list-group-item"><a href="syarat_ketentuan">Syarat dan Ketentuan</a></li>
+                  <li class="list-group-item"><a href="testimoni">Testimoni</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-2">
+            <h3>sosial media</h3>
+            <a href=""><i class="fab fa-facebook-square"></i></a>
+            <a href=""><i class="fab fa-twitter-square"></i></a>
+            <a href=""><i class="fab fa-linkedin"></i></a>
+            <a href=""><i class="fab fa-skype"></i></a>
+            <a href=""><i class="fab fa-youtube"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid footerButtom">
+      <div class="container">
+        <div class="row py-2">
+          <div class="col-md-12">
+            <span>Copyright &copy; Learning <i>Space</i></span>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="<?php echo base_url(); ?>/assets/js/jquery-3.2.1.slim.min.js"></script>
+    <script src="<?php echo base_url(); ?>/assets/js/popper.min.js"></script>
+    <script src="<?php echo base_url(); ?>/assets/js/bootstrap.min.js"></script>
+
+
+        <script src="<?php echo base_url(); ?>/assets/vendors/revolution/js/jquery.themepunch.tools.min.js"></script>
+        <script src="<?php echo base_url(); ?>/assets/vendors/revolution/js/jquery.themepunch.revolution.min.js"></script>
+        <script src="<?php echo base_url(); ?>/assets/vendors/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+        <script src="<?php echo base_url(); ?>/assets/vendors/revolution/js/extensions/revolution.extension.video.min.js"></script>
+        <script src="<?php echo base_url(); ?>/assets/vendors/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+        <script src="<?php echo base_url(); ?>/assets/vendors/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+        <script src="<?php echo base_url(); ?>/assets/vendors/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+        <script src="<?php echo base_url(); ?>/assets/vendors/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+        <!-- Extra plugin css -->
+        <script src="<?php echo base_url(); ?>/assets/vendors/counterup/jquery.waypoints.min.js"></script>
+        <script src="<?php echo base_url(); ?>/assets/vendors/counterup/jquery.counterup.min.js"></script>
+        <script src="<?php echo base_url(); ?>/assets/vendors/counterup/apear.js"></script>
+        <script src="<?php echo base_url(); ?>/assets/vendors/counterup/countto.js"></script>
+        <script src="<?php echo base_url(); ?>/assets/vendors/owl-carousel/owl.carousel.min.js"></script>
+        <script src="<?php echo base_url(); ?>/assets/vendors/parallaxer/jquery.parallax-1.1.3.js"></script>
+        <!--Tweets-->
+        <script src="<?php echo base_url(); ?>/assets/js/theme.js"></script>
+  </body>
+</html>
